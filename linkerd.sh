f57acd42433d7520262c87d7498db7cfc174a6f4
narayananramu <5837582+narayananramu@users.noreply.github.com>
2021-10-22 09:39:51 +0530

    add linkerd script

---
+++ linkerd.sh
@@ -0,0 +1,16 @@
+# create namespace
+kubectl create ns kube-service-mesh-linkerd
+
+# create deployments
+kubectl apply -f linkerd/deployments/main.yaml
+kubectl apply -f linkerd/deployments/service-1.yaml
+kubectl apply -f linkerd/deployments/service-2.yaml
+
+# create services
+kubectl apply -f linkerd/services/main.yaml
+kubectl apply -f linkerd/services/service-1.yaml
+kubectl apply -f linkerd/services/service-2.yaml
+
+# create network policies
+kubectl apply -f linkerd/networkPolicies/service-1.yaml
+kubectl apply -f linkerd/networkPolicies/service-2.yaml
