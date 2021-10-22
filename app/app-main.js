64eacfcde6908707b97b1aba222e451520c02457
narayananramu <5837582+narayananramu@users.noreply.github.com>
2021-10-23 01:28:32 +0530

    use axios and call for service 1 and 2 routes

--- app/app-main.js
+++ app/app-main.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default;
 const express = require('express');
 const cors = require('cors');
 
@@ -6,8 +7,18 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => res.status(200).send('Ok!'));
-app.get('/service-1', (req, res) => res.status(200).send('Ok Service 1!'));
-app.get('/service-2', (req, res) => res.status(200).send('Ok Service 2!'));
+app.get('/service-1', async (req, res) => {
+    const { data } = await axios.get('http://kube-service-mesh-service-1-service.kube-service-mesh-linkerd.svc.cluster.local:3000');
+    return res.status(200).send(data);
+});
+app.get('/service-2', (req, res) => {
+    try {
+        const { data } = await axios.get('http://kube-service-mesh-service-2-service.kube-service-mesh-linkerd.svc.cluster.local:3000');
+        return res.status(200).send(data);
+    } catch (e) {
+        return res.status(500).send(e.toString());
+    }
+});
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000!');
