f11167e6dbf04be32fcc2adda5dd7fb3373ea9f4
narayananramu <5837582+narayananramu@users.noreply.github.com>
2021-10-23 01:35:13 +0530

    add sync to service-2 route

--- app/app-main.js
+++ app/app-main.js
@@ -11,7 +11,7 @@ app.get('/service-1', async (req, res) => {
     const { data } = await axios.get('http://kube-service-mesh-service-1-service.kube-service-mesh-linkerd.svc.cluster.local:3000');
     return res.status(200).send(data);
 });
-app.get('/service-2', (req, res) => {
+app.get('/service-2', async (req, res) => {
     try {
         const { data } = await axios.get('http://kube-service-mesh-service-2-service.kube-service-mesh-linkerd.svc.cluster.local:3000');
         return res.status(200).send(data);
