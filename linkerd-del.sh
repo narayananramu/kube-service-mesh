5d3fe5d3af2fc4b4ccc935e28caed0a9af8acd1f
narayananramu <5837582+narayananramu@users.noreply.github.com>
2021-10-23 10:22:14 +0530

    add delete script

---
+++ linkerd-del.sh
@@ -0,0 +1,6 @@
+kubectl -n kube-service-mesh-linkerd delete ingress --all
+kubectl -n kube-service-mesh-linkerd delete serverauthorization- --all
+kubectl -n kube-service-mesh-linkerd delete server --all
+kubectl -n kube-service-mesh-linkerd delete services --all
+kubectl -n kube-service-mesh-linkerd delete deployments --all
+kubectl ns delete kube-service-mesh-linkerd
