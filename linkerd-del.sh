de79cb925f97e1b13a6b170dc96bb4ca1c5a4336
narayananramu <5837582+narayananramu@users.noreply.github.com>
2021-10-23 11:19:52 +0530

    remove - from serverauthorization

--- linkerd-del.sh
+++ linkerd-del.sh
@@ -1,5 +1,5 @@
 kubectl -n kube-service-mesh-linkerd delete ingress --all
-kubectl -n kube-service-mesh-linkerd delete serverauthorization- --all
+kubectl -n kube-service-mesh-linkerd delete serverauthorization --all
 kubectl -n kube-service-mesh-linkerd delete server --all
 kubectl -n kube-service-mesh-linkerd delete services --all
 kubectl -n kube-service-mesh-linkerd delete deployments --all
